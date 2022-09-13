3a631e8c80c43b252998dda62b1f5231e3f795f5
dhichem <hichem.dimassi@gmail.com>
2022-09-13 16:21:55 +0100

    this is just to test the commit message by the editor
    :)

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello ProGitProgrammers!");
+            Console.WriteLine("This is a test message!");
         }
     }
 }
