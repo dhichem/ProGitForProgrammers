bc2899a9f6723ad91e5ebc34be8ed3be7d64a8b1
dhichem <hichem.dimassi@gmail.com>
2022-09-15 15:53:55 +0100

    commit added 3rd line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -8,6 +8,7 @@ namespace ProGitForProgrammers
         {
             Console.WriteLine("Hello ProGitProgrammers!");
             Console.WriteLine("This is a test message!");
+            Console.WriteLine("we added this line to show the staging area!");
         }
     }
 }
