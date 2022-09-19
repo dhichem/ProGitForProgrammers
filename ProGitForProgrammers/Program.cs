a9e96a19ba914e3bf306724cb16988ab53d64e5c
dhichem <hichem.dimassi@gmail.com>
2022-09-19 17:12:50 +0100

    added Book Class

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -10,5 +10,12 @@ namespace ProGitForProgrammers
             Console.WriteLine("This is a test message!");
             Console.WriteLine("we added this line to show the staging area!");
         }
+
+        public class Book 
+        {
+            public string Title {get; set;}
+            public List<string> Authors {get; set;}
+            public DateTime PublicationDate {get; set;}
+        }
     }
 }
